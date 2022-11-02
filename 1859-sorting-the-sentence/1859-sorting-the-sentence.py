8782edc64a27073e72fec3f8424a1e3e3f872336
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2022-11-02 17:45:49 +0300

    Time: 34 ms (26.41%), Space: 13.7 MB (5.90%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def sortSentence(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        word_arr = s.split()
+        print(word_arr)
+        new_word = []
+        counter = 1
+        sentence = ""
+        while (len(word_arr) != 0):
+            for value in word_arr:
+                if int(value[-1]) == counter:
+                    new_value = value[:-1]
+                    new_word.append(new_value)
+                    print(value)
+                    word_arr.remove(value)
+                    counter = counter + 1
+        for word in new_word:
+            sentence = sentence + word + " "
+        sentence = sentence[:-1]
+        return sentence
+            
+            
+            
+        
