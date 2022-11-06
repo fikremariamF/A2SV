4bcd565b6b71ad6f0cc5c115d674b38188c3af36
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2022-11-06 22:06:06 +0300

    Create Find all anagram in a string.py

---
+++ Find all anagram in a string.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countarr(self,w): 
+        out = [0]*26
+        for c in w:
+            out[ord(c)-ord('a')]+=1
+        return out
+    
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        
+        m = len(p)
+        p_s = self.countarr(p)
+        output = []
+        s_s = self.countarr(s[:m])
+        
+        if s_s == p_s:
+            output = [0]
+        
+        for i in range(1,len(s)-m+1):
+            
+            left = ord(s[i-1])-ord('a') 
+            right = ord(s[i+m-1])-ord('a')
+            s_s[left] = s_s[left] -1
+            s_s[right] = s_s[right] +1
+            
+            if s_s == p_s:
+                output.append(i)
+        return output
