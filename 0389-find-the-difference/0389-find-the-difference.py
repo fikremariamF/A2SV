7ccb16460730112e64e188265ed1ae6dd76a9f1e
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2022-12-20 17:53:36 +0300

    Time: 36 ms (90.73%), Space: 13.9 MB (75.58%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -2,8 +2,6 @@ class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         Scount = Counter(s)
         Tcount = Counter(t)
-        print(Scount)
-        print(Tcount)
         for value in Tcount:
             # print(value not in Tcount)
             if value not in Scount or Scount[value] < Tcount[value]:
