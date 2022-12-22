e74c2b8cedfef121e48161eaf5b6412073be0455
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2022-12-22 08:10:20 +0300

    Time: 1947 ms (87.98%), Space: 69.2 MB (31.84%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -5,16 +5,12 @@ class Solution:
         for match in matches:
             wins[match[0]] = wins.get(match[0],0) + 1
             losses[match[1]] = losses.get(match[1],0) + 1
-        # print(wins)
-        # print(losses)
         winners = []
         lossers = []
         for player in losses:
             if losses[player] == 1:
                 lossers.append(player)
-        # print(losses.keys())
         for player in wins:
-            # print(player)
             if player not in losses.keys():
                 winners.append(player)
         winners.sort()
