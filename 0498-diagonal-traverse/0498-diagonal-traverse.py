8f3b3d5622366ed079cde561e2446752a6e20d4a
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2022-12-30 17:56:40 +0300

    Time: 215 ms (74.02%), Space: 17.7 MB (57.17%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -12,20 +12,10 @@ class Solution:
                 else:
                     row += 1
             else:
-                # print("=======================")
-                # print("in here")
-                # print(prev_func)
-                # print(row, col)
                 if prev_func == "+":
                     while 0 <= row < len(mat) and 0 <= col < len(mat[0]):
-                        # print("+++++++")
-                        # print(mat[row][col])
-                        # print("+++++++")
                         output.append(mat[row][col])
                         if col == 0 or row == len(mat)-1:
-                            # print("----------")
-                            # print(mat[row][col])
-                            # print("----------")
                             break
                         row += 1
                         col -= 1
@@ -36,14 +26,8 @@ class Solution:
                     prev_func = "-"
                 else:
                     while 0 <= row < len(mat) and 0 <= col < len(mat[0]):
-                        # print("+++++++")
-                        # print(mat[row][col])
-                        # print("+++++++")
                         output.append(mat[row][col])
                         if row == 0 or col == len(mat[0]) -1:
-                            # print("----------")
-                            # print(mat[row][col])
-                            # print("----------")
                             break
                         row -= 1
                         col += 1
@@ -53,7 +37,6 @@ class Solution:
                     else:
                         row += 1
                     prev_func = "+"
-        # output.append(mat[-1][-1])
         return output
                 
             
