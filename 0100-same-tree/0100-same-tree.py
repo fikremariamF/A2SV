7c39623b6e3165e905f357a6634011da0009619a
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2023-01-10 17:54:07 +0300

    Time: 41 ms (57.12%), Space: 14 MB (27.10%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        pQueue = deque()
+        qQueue = deque()
+        
+        if p and q:
+            if p.val != q.val:
+                return False
+        
+            pQueue.append(p)
+            qQueue.append(q)
+        
+            proceed = True
+            while pQueue and qQueue:
+                # print(pQueue, qQueue)
+                if len(pQueue) == len(qQueue) and pQueue[0].val == qQueue[0].val:
+                    p = pQueue.popleft()
+                    q = qQueue.popleft()
+                    # print(p.val, q.val)
+                    if type(p.left) == type(q.left):
+                        if p.left:
+                            pQueue.append(p.left)
+                        if q.left:
+                            qQueue.append(q.left)
+                    else:
+                        return False
+                    if type(p.right) == type(q.right):
+                        if p.right:
+                            pQueue.append(p.right)
+                        if q.right:
+                            qQueue.append(q.right)
+                    else:
+                        return False
+                else:
+                    return False
+            return True
+        if type(p) == type(q):
+            # print("here")
+            return True
+        return False
+                
+            
+            
