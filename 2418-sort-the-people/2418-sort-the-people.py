5ae7e0adb52bb3fd5f7fb45453db2fabf958fa34
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2023-01-17 19:57:48 +0300

    Time: 880 ms (8.37%), Space: 21.7 MB (8.95%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,18 +1,19 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        idx = 1
-        while idx < len(names):
-            case = True
-            cur = idx
-            while case and cur > 0:
-                # print(cur)
-                if heights[cur] < heights[cur - 1]:
-                    case = False
-                else:
-                    heights[cur], heights[cur - 1] = heights[cur - 1], heights[cur]
-                    names[cur], names[cur - 1] = names[cur - 1], names[cur]
-                    cur -= 1
-            idx += 1
-         
+        
+        maximum = max(heights)
+        
+        answers = [[] for _ in range(maximum)]
+        # print(len(answers))
+        for idx in range(len(heights)):
+            # print(idx)
+            # print(heights[idx])
+            answers[heights[idx] - 1].append(names[idx])
+        names = []
+        for ans in answers:
+            if ans:
+                for name in ans:
+                    names.append(name)
+        names.reverse()
         return names
             
