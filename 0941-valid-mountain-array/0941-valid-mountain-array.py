d342f5da0a24bb30daedb050a0193ba40983d146
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2023-01-25 17:50:39 +0300

    Time: 263 ms (34.54%), Space: 15.4 MB (63.53%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -4,23 +4,17 @@ class Solution:
         increasing = False
         idx = 0
         while idx < len(arr) - 1:
-            # print(idx)
             if arr[idx] < arr[idx + 1]:
                 increasing = True
             elif arr[idx] >= arr[idx + 1]:
                 break
             idx += 1
-        # print("==============")
         while idx < len(arr) - 1:
-            # print(idx)
             if arr[idx] > arr[idx + 1]:
                 decreasing  = True
             if arr[idx] <= arr[idx + 1]:
                 break
             idx += 1
-        # print(increasing, decreasing)
-        # if arr[-1] < arr[-2]:
-        #     decreasing = True
         if increasing and decreasing and idx == len(arr) - 1:
             return True
         return False
