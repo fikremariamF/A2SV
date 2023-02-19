9a80fb11d0decb61e2e27754707eb0cddf7bc660
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2023-02-19 21:17:58 +0300

    Create SelectionSort.py

---
+++ SelectionSort.py
@@ -0,0 +1,8 @@
+class Solution: 
+    def selectionSort(self, arr,n):
+        #code here
+        for i in range(n):
+            for j in range(i+1, n):
+                if arr[i] > arr[j]:
+                    arr[i], arr[j] = arr[j], arr[i]
+        return arr
