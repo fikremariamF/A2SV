cebb5ae774f96721a11ccb8cbf3646d5340b70a3
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2023-02-19 21:17:24 +0300

    Added solution - LeetHub

--- Selection Sort - GFG/selection-sort.py
+++ Selection Sort - GFG/selection-sort.py
@@ -3,20 +3,14 @@
 class Solution: 
     def select(self, arr, i):
         # code here 
-        idx = 0
-        while idx <len(arr) - 1:
-            if arr[idx] > arr[idx + 1]:
-                arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
-            idx += 1
-            
-        
-    
+        pass
     def selectionSort(self, arr,n):
         #code here
-        arr.sort()
-        # for _ in range(len(arr)):
-        #     self.select(arr, _)
-
+        for i in range(n):
+            for j in range(i+1, n):
+                if arr[i] > arr[j]:
+                    arr[i], arr[j] = arr[j], arr[i]
+        return arr
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
