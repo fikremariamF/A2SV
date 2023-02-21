22e38364b482c2aa9b6e21d2e2274ec393bae113
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2023-02-21 10:56:07 +0300

    Time: 217 ms (37.23%), Space: 14.7 MB (81.09%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.py
+++ 0707-design-linked-list/0707-design-linked-list.py
@@ -12,16 +12,12 @@ class MyLinkedList:
         curr = None
         if self.dummy.next:
             curr = self.dummy.next
-            # print("===",curr.val, "=======")
         for _ in range(index):
             if curr:
-                # print(curr.val)
                 curr = curr.next
         if curr:
-            # print("got",curr.val)
             return curr.val
         else:
-            # print("got nothing")
             return -1
 
     def addAtHead(self, val: int) -> None:
@@ -29,26 +25,15 @@ class MyLinkedList:
         newNode = Node(val)
         newNode.next = temp
         self.dummy.next = newNode
-        # curr = self.dummy.next
-        # while curr:
-        #     print(curr.val)
-        #     curr = curr.next
-        # print("at head added", val)
 
     def addAtTail(self, val: int) -> None:
         curr = self.dummy
         while curr and curr.next:
             curr = curr.next
         curr.next = Node(val)
-        # curr = self.dummy.next
-        # while curr:
-        #     print(curr.val)
-        #     curr = curr.next
-        # print("added at taile", val)
 
     def addAtIndex(self, index: int, val: int) -> None:
         curr = self.dummy.next
-        # print("about to add at index:", index, "-val:", val)
         if index == 0:
             self.addAtHead(val)
             return
@@ -65,31 +50,15 @@ class MyLinkedList:
             return -1
         newNode.next = temp
         curr.next = newNode
-        curr = self.dummy.next
-        # while curr:
-        #     print(curr.val)
-        #     curr = curr.next
-        # print("==============")
     def deleteAtIndex(self, index: int) -> None:
         curr = self.dummy.next
         for _ in range(index - 1):
             if curr:
                 curr = curr.next
-        # print("==to be deleted==")
-        # if curr:
-        #     print(curr.val, index)
-        # print("==to be deleted end==")
         if index == 0:
             print("Last ONE")
             if curr and curr.next:
-                # print("deletting the zeroz")
                 self.dummy.next = curr.next
-                curr = self.dummy.next
-                # while curr:
-                #     print(curr.val)
-                #     curr = curr.next
-                # print("deleted", index)
-                return
             elif curr:
                 self.dummy.next = None
             else:
@@ -99,18 +68,8 @@ class MyLinkedList:
             if curr.next.next:
                 temp = curr.next.next
             curr.next = temp
-            curr = self.dummy.next
-            # while curr:
-            #     print(curr.val)
-            #     curr = curr.next
-            # print("deleted", index)
             return 1
         else:
-            curr = self.dummy.next
-            # while curr:
-            #     print(curr.val)
-            #     curr = curr.next
-            # print("+++++++++")
             return -1
 
 
