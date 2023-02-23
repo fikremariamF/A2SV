3d3f2cb5c5d6dcae0eb6edbf0b9d0992fbf9d475
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2023-02-23 17:45:20 +0300

    Time: 1494 ms (28.49%), Space: 39.4 MB (51.40%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -2,6 +2,8 @@ class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         counts = [0 for char in s]
         letters = [ord(char) for char in s]
+        
+        # add -1 if shifts[2] is 0 else add one to the index
         for shift in shifts:
             if shift[2] == 0:
                 counts[shift[0]] -= 1
@@ -12,6 +14,7 @@ class Solution:
                 if shift[1] + 1 < len(s):
                     counts[shift[1] + 1] -= 1
         letters[0] += counts[0]
+        # calculate the prefix sum
         for idx in range(1 , len(counts)):
             counts[idx] += counts[idx -1]
             letters[idx] += counts[idx]
