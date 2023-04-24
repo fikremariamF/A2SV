2aa3e1a9f1fa235075c0c36ba7d941b270ecba5d
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2023-04-24 23:56:51 +0300

    Time: 149 ms (39.77%), Space: 18.9 MB (5.23%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -0,0 +1,26 @@
+class Solution:
+    def traverse(self, i, j):
+        self.visited.add((i,j))
+        paths = [[1,0], [-1,0], [0,1], [0, -1]]
+        total = 0
+        for path in paths:
+            row, col = path[0], path[1]
+            row += i
+            col += j
+            if 0 <= row < len(self.grid) and 0 <= col < len(self.grid[row]) and (row, col) not in self.visited and self.grid[row][col] == 1:
+                total += self.traverse(row, col)
+        return 1 + total
+    
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        self.grid = grid
+        max_area = 0
+        self.visited = set()
+        for row in range(len(grid)):
+            for col in range(len(grid[row])):
+                if (row,col) not in self.visited and self.grid[row][col] == 1:
+                    area = self.traverse(row, col)
+                    max_area = max(area, max_area)
+                    # print([row, col])
+                    # count += 1
+        # print(count)
+        return max_area
