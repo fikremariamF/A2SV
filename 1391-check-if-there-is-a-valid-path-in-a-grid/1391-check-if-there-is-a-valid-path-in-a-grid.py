09675825d9273772060769f3aa3e08b44c8a9f8a
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2023-05-23 19:16:19 +0300

    Time: 1633 ms (58.22%), Space: 156.8 MB (5.13%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
@@ -12,25 +12,45 @@ class Solution:
         ROWS, COLS = len(grid), len(grid[0])
         
         def isValidMove(r,c): 
-            return r < ROWS and r >= 0 and c < COLS and c >= 0 and (r,c) not in visited
+            return r < ROWS and r >= 0 and c < COLS and c >= 0
         
         
-        q = deque([(0,0)])
-        visited = set()
-        while q:
-            r,c = q.pop()
-            
-            if r == ROWS - 1 and c == COLS - 1:
+        curr = [0,0]
+        visited = {(0,0)}
+        
+        def traverse(x,y):
+            # print(x,y, ROWS, COLS)
+            if (x,y) == (ROWS - 1, COLS - 1):
                 return True
-
-            visited.add((r,c))
-            s = grid[r][c]
-
-            for dr, dc in DIRECTIONS[s]:
-                nr, nc = r+dr, c+dc
-                if isValidMove(nr,nc):
-                    ns = grid[nr][nc]
-                    if (-dr,-dc) in DIRECTIONS[ns]:
-                        q.append((nr, nc))
-
-        return False
+            value = False
+            moves = DIRECTIONS[grid[x][y]]
+            xi,yi = x + moves[0][0], y + moves[0][1]
+            xj,yj = x + moves[1][0], y + moves[1][1]
+            
+            if (x,y) != (0,0):
+                # print(isValidMove(xi,yi), isValidMove(xj,yj))
+                if isValidMove(xi,yi) and isValidMove(xj,yj):
+                    if (xi,yi) in visited:
+                        if (moves[1][0] * -1, moves[1][1] * -1) in DIRECTIONS[grid[xj][yj]] and (xj,yj) not in visited:
+                            visited.add((xj,yj))
+                            value =  traverse(xj,yj)
+                    elif (xj,yj) in visited and (xi,yi) not in visited:
+                        if (moves[0][0] * -1, moves[0][1] * -1) in DIRECTIONS[grid[xi][yi]]:
+                            visited.add((xi,yi))
+                            return traverse(xi,yi)
+                else:
+                    # print(xi,yi,xj,yj, moves)
+                    return False
+            else:
+                if isValidMove(xi,yi):
+                    if (moves[0][0] * -1, moves[0][1] * -1) in DIRECTIONS[grid[xi][yi]]:
+                        visited.add((xi,yi))
+                        if traverse(xi,yi):
+                            value = True
+                if (xj, yj) not in visited and isValidMove(xj,yj) and (moves[1][0] * -1, moves[1][1] * -1) in DIRECTIONS[grid[xj][yj]]:
+                    visited.add((xj,yj))
+                    if traverse(xj,yj):
+                        value = True
+            return value
+        return traverse(0,0)
+        
