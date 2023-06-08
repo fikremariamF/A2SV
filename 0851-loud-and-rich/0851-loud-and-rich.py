0e529ed9bb06ac18d0c1a833c72926ef11175dbc
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2023-06-08 21:50:12 +0300

    Time: 455 ms (85.75%), Space: 26.1 MB (52.90%) - LeetHub

--- 0851-loud-and-rich/0851-loud-and-rich.py
+++ 0851-loud-and-rich/0851-loud-and-rich.py
@@ -1,32 +1,21 @@
 class Solution:
     def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
-        graph = defaultdict(list)
-        n = len(quiet)
-        ans = [999999999] * n
-        visit = set()
-        for rich, poor in richer:
-            graph[poor].append(rich)
-        for i in range(n):
-            if i not in graph:
-                graph[i] = []
-        # print(graph)
-        realans = [-1] * n
-        def dfs(i, start):
-            nonlocal ans
-            if i != start or i == start:
-                if quiet[i] < ans[start]:
-                    ans[start] = quiet[i]
-                    realans[start] = i
-            elif graph[i] == [] and i == start:
-                realans[i] = i
-            if i in visit:
-                return
-            visit.add(i)
-            for nx in graph[i]:
-                dfs(nx, start)
+        neighbours = defaultdict(list)
+        for a,b in richer:
+            neighbours[b].append(a)
+        output = [idx for idx in range(len(quiet))]
         
-        for i in range(n):
-            dfs(i, i)
-            visit.clear()
-
-        return realans
+        visited = set()
+        def dfs(idx):
+            visited.add(idx)
+            for n in neighbours[idx]:
+                if n not in visited:
+                    output[n] = dfs(n)
+                if quiet[output[n]] < quiet[output[idx]]:
+                    output[idx] = output[n]
+            return output[idx]
+        for num in range(len(quiet)):
+            if num not in visited:
+                output[num] = dfs(num)
+            
+        return output
