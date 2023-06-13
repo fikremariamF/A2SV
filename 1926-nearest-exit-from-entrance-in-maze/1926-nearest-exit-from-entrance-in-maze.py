54b173d322c52177297dde58a7886fa28b474682
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2023-06-13 08:32:16 +0300

    Time: 1059 ms (7.23%), Space: 17.9 MB (38.89%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -3,17 +3,13 @@ class Solution:
         moves = [[-1,0], [1,0], [0,-1], [0,1]]
         visited = {(entrance[0], entrance[1])}
         queue = deque([[entrance[0], entrance[1], 0]])
+        
         while queue:
-            # print(visited)
             xi, yi, dist = queue.popleft()
-            # print("in", xi, yi)
             for i,j in moves:
                 xj, yj = xi + i, yi + j
-                # print("op", xj,yj)
                 if 0 <= xj < len(maze) and 0 <= yj < len(maze[0]) and maze[xj][yj] == "." and (xj, yj) not in visited:
-                    # print("valid")
                     if 0==xj or xj == len(maze) - 1 or yj == 0 or yj == len(maze[0]) - 1:
-                        # print(xj,yj)
                         return dist + 1
                     queue.append([xj,yj, dist + 1])
                     visited.add((xj,yj))
