57025330add42fb784b9c08c784fe53d83dfb7fe
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2023-07-21 23:44:43 +0300

    Time: 57 ms (13.02%), Space: 16.7 MB (11.54%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,11 +1,21 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        codes = set([str(i) for i in range(1,27)])
-        if len(s) < 2:
-            return int(s in codes)
-        dp = [0 for _ in range(len(s))]
-        dp[0] = int(s[:1] in codes)
-        dp[1] = int(s[:2] in codes) + (s[0] in codes)*(s[1] in codes)
-        for i in range(2, len(s)):
-            dp[i] = dp[i - 2]*(s[i - 1 : i + 1] in codes) + dp[i - 1]*(s[i] in codes)
-        return dp[-1]
+        @cache
+        
+        def f(s):
+            if not s:
+                return 1
+
+            ans=0
+            
+            for i in range(len(s)):
+                if i>=2:
+                    break
+                sub=s[:i+1]
+                if sub[0]=="0" or (len(sub)==2 and int(sub)>26):
+                    continue
+                ans+=f(s[i+1:])
+            
+            return ans
+
+        return f(s) 
