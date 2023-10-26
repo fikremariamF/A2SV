63563d92a6ffde4395c1ffe211fa8aa736c74c43
fikremariamF <91600690+fikremariamF@users.noreply.github.com>
2023-10-26 16:37:27 +0300

    Time: 103 ms (98.33%), Space: 16.5 MB (68.75%) - LeetHub

---
+++ 0823-binary-trees-with-factors/0823-binary-trees-with-factors.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+        mod = int(1e9 + 7)
+        arr.sort()
+        ans = 0
+        map = {}
+
+        for x in arr:
+            ways = 1
+            max_val = int(x ** 0.5)
+
+            j = 0
+            left = arr[0]
+            while left <= max_val:
+                if x % left != 0:
+                    j += 1
+                    left = arr[j]
+                    continue
+                right = x // left
+                if right in map:
+                    ways = (ways + map[left] * map[right] * (1 if left == right else 2)) % mod
+                j += 1
+                left = arr[j]
+            map[x] = ways
+            ans = (ans + ways) % mod
+
+        return ans
